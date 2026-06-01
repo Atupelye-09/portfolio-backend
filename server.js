9a6fa0a2807ff77461294672ac96caf406169a93
Atupelye Mtitu <atupelyemtitu@gmail.com>
2026-06-01 17:32:28 +0100

    fix syntax error in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-
-require("dotenv").config();zconst express = require("express");
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
 
@@ -12,10 +12,11 @@ app.use(express.json());
 /* DATABASE CONNECTION */
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "portfolio_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 /* CONNECT DATABASE */
@@ -64,6 +65,8 @@ app.post("/contact", (req, res) => {
 
 /* SERVER */
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
